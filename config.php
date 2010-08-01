2d0a5abc0098d8324c94ce34800bd7bd427fd1a5
Michael Rog <michael@michaelrog.com>
2010-08-01 15:38:23 -0700

    updated docs and doc links

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
     $config['name'] = 'RogEE Email-from-Template';
-    $config['version'] = '1.0';
+    $config['version'] = '1.0.1';
     $config['nsm_addon_updater']['versions_xml'] = 'http://bitbucket.org/rogee/email_from_template/raw/tip/versions.xml';
